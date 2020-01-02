1a9ebb36a4a9b5e63513309b40329f596139f7bf
sriRacha21 <arjun.stv@gmail.com>
2020-01-02 19:36:01 +0000

    added a bunch of files to get commando working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rutgers-kun3
+
+A complete rewrite of the Rutgers-kun Discord bot built for the Rutgers Esports Discord.
---
+++ main.js
@@ -0,0 +1,45 @@
+/*  IMPORTS AND GENERAL SETUP   */
+// import sqlite to use as SettingsProvider 
+const sqlite = require('sqlite')
+// import path to join paths in a platform-independent way
+const path = require('path')
+// prepare to read in data from JSON files
+const fs = require('fs')
+const default_encoding = 'utf-8'
+// read in data from JSON file containing default settings for the bot (ClientOptions object)
+const ClientOptions = JSON.parse(fs.readFileSync('default_settings.json', default_encoding))
+// read in data from JSON file containing API keys
+const API_Keys = JSON.parse(fs.readFileSync('api_keys.json', default_encoding))
+// initialize the Discord client
+const Commando = require('discord.js-commando')
+const Client = new Commando.Client(ClientOptions)
+
+/*  EVENTS  */
+// emitted on error, warn, debug
+Client.on('error', console.error)
+Client.on('warn', console.warn)
+Client.on('debug', console.debug)
+Client.on('disconnect', consolw.warn('Websocket disconnected!'))
+Client.on('reconnecting', console.)
+// emitted when bot starts
+Client.on('ready', () => {
+    let outStr = "I'm alive! and running on " + __dirname
+    console.log(outStr)
+    Client.guilds.first().channels.first().send( outStr )
+})
+
+/*  CLEAN UP    */
+// set up SettingsProvider
+Client.setProvider(
+    sqlite.open(
+        path.join(__dirname, 'settings.sqlite3')
+    )
+    .then( db => new Commando.SQLiteProvider(db) )
+    .catch( console.error )
+)
+// set up client's command registry
+Client.registry
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname,'commands'))
+// log in
+Client.login(API_Keys.token)
