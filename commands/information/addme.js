822a7bbb1aa6ec3f199801be17a78dc25fd48018
sriRacha21 <arjun.stv@gmail.com>
2020-02-25 19:42:35 -0500

    added embeds to help users add bot to servers and initiate a pr

---
+++ commands/information/addme.js
@@ -0,0 +1,35 @@
+const Commando = require('discord.js-commando')
+const { generateDefaultEmbed } = require('../../helpers/generateDefaultEmbed')
+const { getRandomElement } = require('../../helpers/getRandom')
+
+module.exports = class AddMeCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'addme',
+            group: 'information',
+            memberName: 'addme',
+            description: 'Add me to another server!',
+        })
+    }
+
+
+    async run( msg ) {
+        const embed = generateDefaultEmbed({
+            author: 'Aww do you like me that much?',
+            title: 'Add me to another server!',
+            clientUser: this.client.user,
+            msg: msg,
+            guild: msg.guild,
+        })
+
+        const emote = msg.guild ? getRandomElement(msg.guild.emojis.array()) : getRandomElement(this.client.emojis.array())
+        embed.setThumbnail(emote.url)
+
+        this.client.generateInvite()
+        .then(link => {
+            embed.setURL(link)
+            msg.channel.send( embed )
+        })
+        .catch(e => { if(e) msg.channel.send(`Error encountered: ${e}`) })
+	}
+}
---
+++ commands/information/pullrequest.js
@@ -0,0 +1,29 @@
+const Commando = require('discord.js-commando')
+const { generateDefaultEmbed } = require('../../helpers/generateDefaultEmbed')
+
+module.exports = class PullRequestCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'pullrequest',
+            aliases: [ 'pr' ],
+            group: 'information',
+            memberName: 'pullrequest',
+            description: 'Want to add something? Make a pull request.',
+        })
+    }
+
+
+    async run( msg ) {
+        const embed = generateDefaultEmbed({
+            author: 'Have a suggestion?',
+            title: 'Make a pull request!',
+            thumbnail: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+            clientUser: this.client.user,
+            msg: msg,
+        })
+        .setURL('https://github.com/sriRacha21/Rutgers-kun3/pulls')
+        .setDescription( 'Or create an [issue](https://github.com/sriRacha21/Rutgers-kun3/issues).' )
+
+        return msg.channel.send( embed )
+	}
+}
