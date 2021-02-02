b4fbe6f06dde41176df1e8dc11f12e130bb4ac71
Arjun Srivastav <arjun.stv@gmail.com>
2021-02-02 05:16:23 -0500

    Removed unused helpers

--- helpers/payMe.js
+++
@@ -1,6 +0,0 @@
-function payMe( msg, commands ) {
-    const bmacCommand = commands.find(c => c.name === 'bmac');
-    if ( msg.cleanContent.match(/\bpay me\b/i) && bmacCommand ) { bmacCommand.run( msg, null, false, null ); }
-}
-
-exports.payMe = payMe;
